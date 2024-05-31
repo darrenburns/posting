3805189b8463ddc679b2b3b134e27a200b61a5cf
Darren Burns <darrenb900@gmail.com>
2024-05-31 01:02:31 +0100

    Styling

---
+++ src/postling/suggesters.py
@@ -0,0 +1,11 @@
+# Maps lowercased HTTP headers to lists of suggestions for the header's value.
+HEADER_SUGGESTION_LISTS = {
+    "content-type": [
+        "text/",
+        "text/html",
+        "application/",
+        "application/json",
+        "application/xml",
+        "application/x-www-form-urlencoded",
+    ],
+}
