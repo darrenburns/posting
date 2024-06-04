e2a8c8ba491da560ff479eb63e9f3504d012b302
Darren Burns <darrenb900@gmail.com>
2024-06-04 14:22:55 +0100

    Response body bar

--- src/posting/widgets/response/response_area.py
+++ src/posting/widgets/response/response_area.py
@@ -1,9 +1,11 @@
 import httpx
+from textual import on
 from textual.app import ComposeResult
 from textual.binding import Binding
-from textual.containers import Vertical
+from textual.containers import Vertical, Horizontal
 from textual.reactive import Reactive, reactive
-from textual.widgets import TabbedContent, TabPane
+from textual.widgets import TabbedContent, TabPane, Select, Checkbox, Switch, Label
+from textual.widgets._toggle_button import ToggleButton
 
 from posting.widgets.response.cookies_table import CookiesTable
 from posting.widgets.response.response_body import ResponseTextArea
@@ -15,6 +17,46 @@ class ResponseTabbedContent(TabbedContent):
         Binding("down,j", "app.focus_next", "Focus next", show=False),
         Binding("up,k", "app.focus_previous", "Focus previous", show=False),
     ]
+
+
+class ResponseBodyHeaderBar(Horizontal):
+    """The bar that appears above the response body, allowing
+    you to customise the syntax highlighting, wrapping, line numbers,
+    etc.
+    """
+
+    DEFAULT_CSS = """\
+    ResponseBodyHeaderBar {
+        dock: bottom;
+        height: 1;
+        width: 1fr;
+        background: $accent;
+        
+        &:disabled {
+            background: transparent;
+        }
+        
+        & Select {
+            width: 8;
+            & SelectCurrent {
+                width: 8;
+            }
+            & SelectOverlay {
+                width: 16;
+            }
+        }
+        
+        & Checkbox {
+            margin-left: 1;
+        }
+    }
+    """
+
+    def compose(self) -> ComposeResult:
+        with Horizontal(classes="dock-left w-auto"):
+            yield Select(prompt="Content type", value="json", allow_blank=False,
+                         options=[("JSON", "json"), ("HTML", "html"), ("XML", "xml")])
+            yield Checkbox(label="Wrap", value=True, button_first=False, id="response-wrap-checkbox")
 
 
 class ResponseArea(Vertical):
@@ -44,6 +86,7 @@ class ResponseArea(Vertical):
     def compose(self) -> ComposeResult:
         with ResponseTabbedContent(disabled=self.response is None):
             with TabPane("Body", id="response-body-pane"):
+                yield ResponseBodyHeaderBar()
                 yield ResponseTextArea(language="json", read_only=True)
             with TabPane("Headers", id="response-headers-pane"):
                 yield ResponseHeadersTable()
@@ -72,6 +115,7 @@ class ResponseArea(Vertical):
         cookies_table = self.cookies_table
         cookies_table.clear()
         cookies_table.add_rows([(name, value) for name, value in response.cookies.items()])
+
         print(response.cookies)
 
         if response.status_code < 300:
@@ -86,6 +130,11 @@ class ResponseArea(Vertical):
         )
 
         self.border_subtitle = f"{response.elapsed.total_seconds() * 1000:.2f} ms"
+
+    @on(Checkbox.Changed, selector="#response-wrap-checkbox")
+    def wrap_toggled(self, event: Checkbox.Changed) -> None:
+        print("changing wrap")
+        self.body_text_area.soft_wrap = event.value
 
     @property
     def body_text_area(self) -> ResponseTextArea:
