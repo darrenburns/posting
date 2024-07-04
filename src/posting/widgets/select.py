b8c9f11f4d8dc29edb94e73fa866f0163817dbfc
Darren Burns <darrenb900@gmail.com>
2024-07-04 11:36:27 +0100

    Add vim key support to select widgets

--- src/posting/widgets/select.py
+++ src/posting/widgets/select.py
@@ -1,8 +1,24 @@
 from typing import TypeVar
+from textual.binding import Binding
 from textual.widgets import Select
+from textual.widgets._select import SelectOverlay
 
 T = TypeVar("T")
 
 
 class PostingSelect(Select[T], inherit_bindings=False):
-    BINDINGS = [("enter,space", "show_overlay")]
+    BINDINGS = [
+        Binding("enter,space", "show_overlay"),
+        Binding("up,k", "cursor_up"),
+        Binding("down,j", "cursor_down"),
+    ]
+
+    def action_cursor_up(self):
+        self.select_overlay.action_cursor_up()
+
+    def action_cursor_down(self):
+        self.select_overlay.action_cursor_down()
+
+    @property
+    def select_overlay(self) -> SelectOverlay:
+        return self.query_one(SelectOverlay)
