3584c0ce3583071131b085cdb73e75dff8b17118
Darren Burns <darrenb900@gmail.com>
2024-07-20 22:11:33 +0100

    Some snapshot tests around loading requests from disk

--- tests/test_snapshots.py
+++ tests/test_snapshots.py
@@ -203,3 +203,28 @@ class TestUserInterfaceShortcuts:
             await pilot.press("ctrl+m")  # reset sections
 
         assert snap_compare(POSTING_MAIN, run_before=run_before)
+
+
+@use_config("general.yaml")
+@patch_env("POSTING_FOCUS__ON_STARTUP", "collection")
+class TestLoadingRequest:
+    def test_request_loaded_into_view__headers(self, snap_compare):
+        """Check that the request headers are loaded into the view."""
+
+        async def run_before(pilot: Pilot):
+            # Navigate to 'get one post' and select it.
+            await pilot.press("J", "J", "j")
+            await pilot.press("enter")
+
+        assert snap_compare(POSTING_MAIN, run_before=run_before, terminal_size=(80, 34))
+
+    def test_request_loaded_into_view__body(self, snap_compare):
+        """Check that the request body is loaded into the view."""
+
+        async def run_before(pilot: Pilot):
+            # Navigate to 'POST one post' and select it.
+            await pilot.press("J", "J", "j", "j")
+            await pilot.press("enter")
+            await pilot.press("ctrl+o", "w")  # jump to 'Body' tab
+
+        assert snap_compare(POSTING_MAIN, run_before=run_before, terminal_size=(80, 34))
