957500b83cd9736e5f432d68b770cb8ff3f35ebf
Liang-Shih Lin <liangshihlin@gmail.com>
2024-09-14 12:04:52 +0800

    Add request types

--- src/posting/importing/postman.py
+++ src/posting/importing/postman.py
@@ -1,7 +1,8 @@
 from pathlib import Path
-from typing import List
+from typing import Any, List, Optional
 import json
 import os
+from pydantic import BaseModel
 import yaml
 
 from rich.console import Console
@@ -9,15 +10,51 @@ from rich.console import Console
 from posting.collection import APIInfo, Collection
 
 
-def create_env_file(
-    path: Path, env_filename: str, variables: list[dict[str, str]]
-) -> Path:
-    env_content: list[str] = []
+class Variable(BaseModel):
+    key: str
+    value: str
+    type: Optional[str] = None
+    disabled: Optional[bool] = None
+
+
+class Body(BaseModel):
+    mode: str
+    options: dict
+    raw: Optional[str] = None
+
+
+class Url(BaseModel):
+    raw: str
+    host: Optional[List[str]] = None
+    path: Optional[List[str]] = None
+    query: Optional[List[Variable]] = None
+
+
+class PostmanRequest(BaseModel):
+    method: str
+    url: str | Url
+    header: Optional[List[Variable]] = None
+    description: Optional[str] = None
+    body: Optional[Body] = None
+
+
+class RequestItem(BaseModel):
+    name: str
+    item: Optional[List["RequestItem"]] = None
+    request: Optional[PostmanRequest] = None
+
+
+class PostmanCollection(BaseModel):
+    info: dict[str, str]
+    item: List[RequestItem]
+    variable: List[Variable]
+
+
+def create_env_file(path: Path, env_filename: str, variables: List[Variable]) -> Path:
+    env_content: List[str] = []
 
     for var in variables:
-        key = var["key"]
-        value = var["value"]
-        env_content.append(f"{key!r}={value!r}")
+        env_content.append(f"{var.key!r}={var.value!r}")
 
     env_file = path / env_filename
     env_file.write_text("\n".join(env_content))
@@ -25,52 +62,57 @@ def create_env_file(
 
 
 def generate_directory_structure(
-    items: List[dict], current_path: str = "", base_path: Path = Path("")
+    items: List[RequestItem], current_path: str = "", base_path: Path = Path("")
 ) -> List[str]:
     directories = []
     for item in items:
-        if "item" in item:
-            folder_name = item["name"]
+        if item.item is not None:
+            folder_name = item.name
             new_path = f"{current_path}/{folder_name}" if current_path else folder_name
             full_path = Path(base_path) / new_path
             os.makedirs(str(full_path), exist_ok=True)
             directories.append(str(full_path))
-            generate_directory_structure(item["item"], new_path, base_path)
-        elif "request" in item:
-            request_name = item["name"]
+            generate_directory_structure(item.item, new_path, base_path)
+        if item.request is not None:
+            request_name = item.name
             file_name = f"{request_name}.posting.yaml"
             full_path = Path(base_path) / current_path / file_name
             create_request_file(full_path, item)
     return directories
 
 
-def create_request_file(file_path: Path, request_data: dict):
-    yaml_content = {
-        "name": request_data["name"],
-        "description": request_data.get("description", ""),
-        "method": request_data["request"]["method"],
-        "url": request_data["request"]["url"]["raw"],
+def create_request_file(file_path: Path, request_data: RequestItem):
+    yaml_content: dict[str, Any] = {
+        "name": request_data.name,
     }
 
-    # Add body if present
-    if "body" in request_data["request"]:
-        yaml_content["body"] = {
-            "content": request_data["request"]["body"].get("raw", "")
-        }
+    if request_data.request is not None:
+        yaml_content["method"] = request_data.request.method
 
-    # Add headers
-    if "header" in request_data["request"]:
-        yaml_content["headers"] = [
-            {"name": header["key"], "value": header["value"]}
-            for header in request_data["request"]["header"]
-        ]
+        if request_data.request.header is not None:
+            yaml_content["headers"] = [
+                {"name": header.key, "value": header.value}
+                for header in request_data.request.header
+            ]
 
-    # Add query params
-    if "query" in request_data["request"]["url"]:
-        yaml_content["params"] = [
-            {"name": param["key"], "value": param["value"]}
-            for param in request_data["request"]["url"]["query"]
-        ]
+        if isinstance(request_data.request.url, Url):
+            yaml_content["url"] = request_data.request.url.raw
+            if request_data.request.url.query is not None:
+                yaml_content["params"] = [
+                    {"name": param.key, "value": param.value}
+                    for param in request_data.request.url.query
+                ]
+        else:
+            yaml_content["url"] = request_data.request.url
+
+        if request_data.request.description is not None:
+            yaml_content["description"] = request_data.request.description
+
+        if (
+            request_data.request.body is not None
+            and request_data.request.body.raw is not None
+        ):
+            yaml_content["body"] = {"content": request_data.request.body.raw}
 
     # Write YAML file
     with open(file_path, "w") as f:
@@ -85,12 +127,14 @@ def import_postman_spec(
 
     spec_path = Path(spec_path)
     with open(spec_path, "r") as file:
-        spec = json.load(file)
+        spec_dict = json.load(file)
+
+    spec = PostmanCollection(**spec_dict)
 
     info = APIInfo(
-        title=spec["info"]["name"],
-        description=spec["info"].get("description", "No description"),
-        specSchema=spec["info"]["schema"],
+        title=spec.info["name"],
+        description=spec.info.get("description", "No description"),
+        specSchema=spec.info["schema"],
         version="2.0.0",
     )
 
@@ -102,13 +146,13 @@ def import_postman_spec(
             else output_path.parent
         )
 
-    env_file = create_env_file(base_dir, f"{info.title}.env", spec["variable"])
+    env_file = create_env_file(base_dir, f"{info.title}.env", spec.variable)
     console.print(f"Created environment file {str(env_file)!r}.")
 
     main_collection = Collection(path=spec_path.parent, name="Postman Test")
 
     # Create the directory structure like Postman's request folders
-    directories = generate_directory_structure(spec["item"], base_path=base_dir)
+    directories = generate_directory_structure(spec.item, base_path=base_dir)
     console.print("Finished importing postman collection.")
 
     return main_collection, env_file, directories
