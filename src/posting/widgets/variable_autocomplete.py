df2e28ac42974aed09ae687cbfc9c8a27a6d7397
Darren Burns <darrenb900@gmail.com>
2024-10-12 11:21:58 +0100

    Bump version to 2.0.0b1, fix completion strategy impl

--- src/posting/widgets/variable_autocomplete.py
+++ src/posting/widgets/variable_autocomplete.py
@@ -60,11 +60,14 @@ class VariableAutoComplete(AutoComplete):
         else:
             return super().get_candidates(target_state)
 
-    def _completion_strategy(
-        self, value: str, target_state: TargetState
-    ) -> TargetState:
+    def _completion_strategy(self, value: str, target_state: TargetState) -> None:
+        """Modify the target state to reflect the completion.
+
+        Only works in Inputs for now.
+        """
         cursor = target_state.selection.end[1]
         text = target_state.text
+        target: Input = self.target
         if is_cursor_within_variable(cursor, text):
             # Replace the text from the variable start
             # with the completion text.
@@ -73,18 +76,11 @@ class VariableAutoComplete(AutoComplete):
             old_value = text
             new_value = old_value[:start] + value + old_value[end:]
 
-            # Move the cursor to the end of the inserted text
-            new_column = start + len(value)
-            return TargetState(
-                text=new_value,
-                selection=Selection.cursor((0, new_column)),
-            )
+            target.value = new_value
+            target.cursor_position = start + len(value)
         else:
-            # Replace the entire contents
-            return TargetState(
-                text=value,
-                selection=Selection.cursor((0, len(value))),
-            )
+            target.value = value
+            target.cursor_position = len(value)
 
     def _search_string(self, target_state: TargetState) -> str:
         cursor = target_state.selection.end[1]
