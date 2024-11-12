acf58eb37b80fd1c8ffc9f2c5db7fa3230fac84d
Devin Droddy <devin.droddy@gmail.com>
2024-11-12 12:47:01 -0500

    temp remove home-manager module + use easyOverlay

--- flake.nix
+++ flake.nix
@@ -10,64 +10,17 @@
   };
 
   outputs = inputs @ {flake-parts, ...}:
-    flake-parts.lib.mkFlake {inherit inputs;} rec {
-      imports = [flake-parts.flakeModules.modules];
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [flake-parts.flakeModules.modules flake-parts.flakeModules.easyOverlay];
       systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
-      flake.overlays.default = final: prev: (
-        inputs.textual-autocomplete.overlays.default final prev
-        // {
-          posting = final.callPackage ./package.nix {};
-        }
-      );
-      flake.modules.homeManager.default = {
-        config,
-        lib,
-        pkgs,
-        ...
-      }: let
-        inherit (lib) mkOption mkIf mkEnableOption;
-        cfg = config.programs.posting;
-      in {
-        nixpkgs.overlays = [flake.overlays.default];
-        home.packages = [pkgs.posting];
-        # options.programs.posting = {
-        #   enable = mkEnableOption "Posting API client";
-        #   package = mkOption {
-        #     type = lib.types.package;
-        #     description = "The posting package to use.";
-        #     default = pkgs.callPackage ./package.nix {};
-        #     defaultText = "outputs.packages.\${system}.default";
-        #   };
-        #   settings = mkOption {
-        #     type = (pkgs.formats.yaml {}).type;
-        #     default = {};
-        #     example = {
-        #       theme = "galaxy";
-        #       layout = "horizontal";
-        #       response.prettify_json = false;
-        #       heading = {
-        #         visible = true;
-        #         show_host = false;
-        #       };
-        #     };
-        #     description = "Posting configuration settings. See <https://github.com/darrenburns/posting/blob/main/docs/guide/configuration.md>";
-        #   };
-        # };
-        #
-        # config =
-        #   mkIf cfg.enable {
-        #     home.packages = [cfg.package];
-        #     home.file.".config/posting/config.yaml".text = lib.genrators.toYAML cfg.settings;
-        #   }
-        #   // {
-        #     nixpkgs.overlays = [inputs.textual-autocomplete.overlays.default];
-        #   };
-      };
       perSystem = {
         pkgs,
         system,
         ...
-      }: {
+      }: rec {
+        overlayAttrs = {
+          posting = packages.default;
+        };
         _module.args.pkgs = import inputs.nixpkgs {
           inherit system;
           overlays = [
