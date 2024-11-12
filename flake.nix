42e7a229b81271e9796d8da50b4f4ba33ea454a1
Devin Droddy <devin.droddy@gmail.com>
2024-11-12 13:08:51 -0500

    use pkgs.extend to add textual-autocomplete to pkgs

--- flake.nix
+++ flake.nix
@@ -10,17 +10,54 @@
   };
 
   outputs = inputs @ {flake-parts, ...}:
-    flake-parts.lib.mkFlake {inherit inputs;} {
-      imports = [flake-parts.flakeModules.modules flake-parts.flakeModules.easyOverlay];
+    flake-parts.lib.mkFlake {inherit inputs;} rec {
+      imports = [flake-parts.flakeModules.modules];
       systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+      flake.overlays.default = final: prev: (
+        inputs.textual-autocomplete.overlays.default final prev
+        // {
+          posting = final.callPackage ./package.nix {};
+        }
+      );
+      flake.modules.homeManager.default = {
+        config,
+        lib,
+        pkgs,
+        ...
+      }: let
+        inherit (lib) mkOption mkIf mkEnableOption mkPackageOption;
+        cfg = config.programs.posting;
+        pkgs' = pkgs.extend flake.overlays.default;
+      in {
+        options.programs.posting = {
+          enable = mkEnableOption "Posting API client";
+          package = mkPackageOption pkgs' "posting";
+          settings = mkOption {
+            type = (pkgs'.formats.yaml {}).type;
+            default = {};
+            example = {
+              theme = "galaxy";
+              layout = "horizontal";
+              response.prettify_json = false;
+              heading = {
+                visible = true;
+                show_host = false;
+              };
+            };
+            description = "Posting configuration settings. See <https://github.com/darrenburns/posting/blob/main/docs/guide/configuration.md>";
+          };
+        };
+
+        config = mkIf cfg.enable {
+          home.packages = [cfg.package];
+          home.file.".config/posting/config.yaml".text = lib.genrators.toYAML cfg.settings;
+        };
+      };
       perSystem = {
         pkgs,
         system,
         ...
-      }: rec {
-        overlayAttrs = {
-          posting = packages.default;
-        };
+      }: {
         _module.args.pkgs = import inputs.nixpkgs {
           inherit system;
           overlays = [
