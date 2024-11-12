0eb7d9745eebe45e93903cae30957d69b8a1d2f7
Devin Droddy <devin.droddy@gmail.com>
2024-11-12 13:19:11 -0500

    use toJSON instead of toYAML
    toYAML is just a wrapper around toJSON anyways

--- flake.nix
+++ flake.nix
@@ -50,7 +50,7 @@
 
         config = mkIf cfg.enable {
           home.packages = [cfg.package];
-          home.file.".config/posting/config.yaml".text = lib.generators.toYAML cfg.settings;
+          home.file.".config/posting/config.yaml".text = builtins.toJSON cfg.settings;
         };
       };
       perSystem = {
