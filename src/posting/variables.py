788ae20b923849a1c33e4f4eb3653cf47426649a
Thomas Ryan <18319621+ThomasJRyan@users.noreply.github.com>
2024-12-08 20:31:20 -0500

    Changed SharedVariables to use Paths as keys and added seemless saving

--- src/posting/variables.py
+++ src/posting/variables.py
@@ -4,8 +4,9 @@ from functools import lru_cache
 import re
 import os
 from pathlib import Path
-from dotenv import dotenv_values
+from dotenv import dotenv_values, set_key, unset_key
 
+from posting.locations import data_directory
 
 _VARIABLES_PATTERN = re.compile(
     r"\$(?:([a-zA-Z_][a-zA-Z0-9_]*)|{([a-zA-Z_][a-zA-Z0-9_]*)})"
@@ -13,16 +14,33 @@ _VARIABLES_PATTERN = re.compile(
 
 
 class SharedVariables:
-    def __init__(self, current_env: str = "default.env") -> None:
-        self._current_env = current_env
-        self._variables: dict[str, object] = {}
+    def __init__(self, current_env: str = data_directory() / "default.env") -> None:
+        self._current_env: Path = current_env
+        self._variables: dict[Path, dict[str, object]] = {}
 
-    def get(self, env: str | None = None) -> dict[str, dict[str, object]]:
+    def get(self, env: str | None = None) -> dict[str, object]:
+        """Get the variables for the current environment, or for a specific 
+        environment if provided.
+
+        Args:
+            env (str | None, optional): Environment to get variables for. Defaults to None.
+
+        Returns:
+            dict[str, object]: Variables for the environment.
+        """
         if env:
             return self._variables.get(env, {}).copy()
         return self._variables.get(self._current_env, {}).copy()
 
     def set(self, variables: dict[str, object], env: str | None = None) -> None:
+        """Set the variables for the current environment, or for a specific
+        environment if provided.
+
+        Args:
+            variables (dict[str, object]): The variables to set.
+            env (str | None, optional): The environment to set the variables to. 
+                                        Defaults to None.
+        """
         if env:
             self._variables[env] = variables
         else:
@@ -30,31 +48,51 @@ class SharedVariables:
 
     def update(self, new_variables: dict[str, object], 
                env: str | None = None ) -> None:
+        """Update the current variables with new values. If a key already exists,
+        its value will be updated. If it doesn't exist, it will be added.
+
+        Args:
+            new_variables (dict[str, object]): The new variables to update or add.
+            env (str | None, optional): The environment to update the variables in. 
+                                        Defaults to None.
+        """
         if env:
             self._variables[env].update(new_variables)
         else:
             self._variables[self._current_env].update(new_variables)
         
     def remove(self, key: str, env: str | None = None) -> None:
+        """Remove a variable from the current variables. If an environment is provided,
+        remove the variable from that environment instead.
+
+        Args:
+            key (str): The key of the variable to remove.
+            env (str | None, optional): The environment to remove the variable from. 
+                                        Defaults to None.
+        """
         if env:
             self._variables[env].pop(key, None)
         else:
             self._variables[self._current_env].pop(key, None)
             
-    def get_envs(self) -> list[str]:
+    def get_envs(self) -> list[Path]:
+        """ Get the names of the environments that have been loaded. """
         return list(self._variables.keys())
 
     def set_current_env(self, env: str) -> None:
+        """ Set the current environment name. """
         self._current_env = env
     
     def get_current_env(self) -> str:
+        """ Get the current environment name. """
         return self._current_env
-
+    
 VARIABLES = SharedVariables()
 
 
-def get_variables() -> dict[str, object]:
-    return VARIABLES.get()
+def get_variables(env: str | None = None) -> dict[str, object]:
+    """ Get the variables for the current environment. """
+    return VARIABLES.get(env)
 
 
 def load_variables(
@@ -77,29 +115,59 @@ def load_variables(
         overlay_variables: Additional variables to overlay on top of the result.
     """
 
+    # If we already have variables and we're not avoiding the cache, return them.
     existing_variables = get_variables()
     if existing_variables and not avoid_cache:
         return {key: value for key, value in existing_variables.items()}
 
+    # Load the variables from the environment files.
     envs: dict[str, dict[str, object]] = {}
     for file in environment_files:
-        variables = envs.setdefault(file.name, {})
+        variables = envs.setdefault(file, {})
         for key, value in dotenv_values(file).items():
             variables[key] = value
     
-    
+    # If we're using the host environment, add those variables under the 
+    # "host_environment" key.
     if use_host_environment:
-        variables = envs.setdefault("host_environment", {})
+        variables = envs.setdefault(Path("/host_environment"), {})
         host_env_variables = {key: value for key, value in os.environ.items()}
         variables.update({**variables, **host_env_variables})
 
+    # Provide a default environment
+    default_env_path = data_directory() / "default.env"
+    
+    # If the default environment file doesn't exist, create it.
+    if not default_env_path.exists():
+        with open(default_env_path, "w") as f:
+            f.write("")
+    
+    # Load the default environment variables
+    variables = envs.setdefault(default_env_path, {})
+    for key, value in dotenv_values(default_env_path).items():
+        variables[key] = value
+        
+    # For each environment, set the variables in the shared variables object.
     for env, variables in envs.items():
         VARIABLES.set(variables, env=env)
-        
-    VARIABLES.set_current_env(environment_files[0].name)
+  
+    # Set the current environment to the first environment file if it we have any.
+    # Or set it to the host environment if we're using that.
+    if environment_files:
+        VARIABLES.set_current_env(environment_files[0])
+    elif use_host_environment:
+        VARIABLES.set_current_env(Path("/host_environment"))
         
     return get_variables()
 
+def save_variables(env: str | None = None) -> None:
+    env = env or VARIABLES.get_current_env()
+    
+    if env == "host_environment":
+        return
+    
+    for key, value in get_variables(env).items():
+        set_key(str(env), key, value)
 
 def update_variables(new_variables: dict[str, object]) -> None:
     """Update the current variables with new values.
@@ -111,22 +179,31 @@ def update_variables(new_variables: dict[str, object]) -> None:
         new_variables: A dictionary containing the new variables to update or add.
     """
     VARIABLES.update(new_variables)
+    save_variables()
     
 def remove_variable(key: str) -> None:
-    """Remove a variable from the shared variables.
+    """Remove a variable from the current variables.
 
     Args:
         key: The key of the variable to remove.
     """
     VARIABLES.remove(key)
+    unset_key(str(VARIABLES.get_current_env()), key)
 
-def get_environments() -> list[str]:
+def get_environments() -> list[Path]:
+    """Get the paths of the environments that have been loaded.
+
+    Returns:
+        list[str]: A list of environment paths.
+    """
     return list(VARIABLES.get_envs())
 
 def set_current_environment(env: str) -> None:
+    """Set the current environment name."""
     VARIABLES.set_current_env(env)
     
 def get_current_environment() -> str:
+    """Get the current environment name."""
     return VARIABLES._current_env
 
 @lru_cache()
