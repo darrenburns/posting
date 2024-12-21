14c312c6a36a922aacb6d4dd681f963544605ba2
Darren Burns <darrenb900@gmail.com>
2024-12-21 12:24:58 +0000

    Displaying reply count

--- src/posting/widgets/websocket/replies.py
+++ src/posting/widgets/websocket/replies.py
@@ -3,10 +3,17 @@ import datetime
 from textual.app import ComposeResult
 from textual.containers import Vertical
 from textual.message import Message
+from textual.reactive import reactive
 from textual.widgets import Label, RichLog
 
 
 class Replies(Vertical):
+    """
+    A widget for displaying replies from the server.
+    """
+
+    number_of_replies: int = reactive(0)
+
     @dataclass
     class Incoming(Message):
         message: str
@@ -14,11 +21,14 @@ class Replies(Vertical):
 
     def compose(self) -> ComposeResult:
         self.border_title = "Incoming"
-        yield Label("Replies")
         yield RichLog(id="replies-rich-log")
 
     def add_reply(self, message: str) -> None:
         self.replies_rich_log.write(message)
+        self.number_of_replies += 1
+
+    def watch_number_of_replies(self, number: int) -> None:
+        self.border_subtitle = f"{number} messages received"
 
     @property
     def replies_rich_log(self) -> RichLog:
